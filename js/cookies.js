7465eb7dff066360bccb2453b8148421b8948217
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-03-12 18:15:03 -0600

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -3,4 +3,5 @@ const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 const d = new Date();
 let day = days[d.getDay()];
 
+const fruits = [
 document.cookie = 'sconName='+changedName+'; expires='day', 1 Jan 2070 13:47:11 UTC; path=/'
